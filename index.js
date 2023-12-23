8aea042cfcbed89497dba2de106580e1e75c19ec
Felix <felixomosh7@gmail.com>
2023-12-23 17:44:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    { year: "2019", result: "W" },
+    { year: "2020", result: "L" },
+    { year: "2021", result: "W" },
+    { year: "2022", result: "W" },
+    { year: "2023", result: "L" },
+];
+
+function superbowlWin(records) {
+    // Use the find method to search for a "W" in the records
+    const winningRecord = records.find(record => record.result === "W");
+
+    return winningRecord ? winningRecord.year : undefined;
+}
+
+const winningYear = superbowlWin(record);
+console.log(winningYear); 
